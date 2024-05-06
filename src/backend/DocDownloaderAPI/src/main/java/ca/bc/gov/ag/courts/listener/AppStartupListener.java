14396cfdb67c89240e22821e67a2d49cbcda6f57
Shaun Millar <shaun.millar@gov.bc.ca>
2024-05-06 13:47:38 -0700

    Added startup listener

---
+++ src/backend/DocDownloaderAPI/src/main/java/ca/bc/gov/ag/courts/listener/AppStartupListener.java
@@ -0,0 +1,30 @@
+package ca.bc.gov.ag.courts.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import ca.bc.gov.ag.courts.config.AppProperties;
+
+@Component
+public class AppStartupListener implements ApplicationListener<ApplicationReadyEvent> {
+	
+	static final Logger log = LoggerFactory.getLogger(AppStartupListener.class);
+	
+	@Autowired
+	AppProperties props; 
+	
+	/**
+	 * This event is executed as late as conceivably possible to indicate that the
+	 * application is ready to service requests.
+	 */
+	@Override
+	public void onApplicationEvent(final ApplicationReadyEvent event) {
+		
+		log.info("BC Courts Doc Downlaoder API startuping up. Version: " + props.getApplicationVersion());
+		return;
+	}
+}
