e63249fbf5d5623af9ed910c194a01941b32541a
Shaun Millar <shaun.millar@gov.bc.ca>
2024-06-14 15:46:42 -0700

    Fixed Async getDocStatus

--- src/backend/DocDownloaderAPI/src/main/java/ca/bc/gov/ag/courts/service/RedisCacheClientServiceImpl.java
+++ src/backend/DocDownloaderAPI/src/main/java/ca/bc/gov/ag/courts/service/RedisCacheClientServiceImpl.java
@@ -71,9 +71,63 @@ public class RedisCacheClientServiceImpl implements RedisCacheClientService {
 
 		return CompletableFuture.completedFuture(responseEntity);
 	}
-	
+
 	@Override
-	@Async
+	@Retryable(retryFor = RestClientException.class, maxAttemptsExpression = "${application.net.max.retries}", backoff = @Backoff(delayExpression = "${application.net.delay}"))
+	public CompletableFuture<ResponseEntity<String>> createJob(Job job) throws Exception {
+		
+		logger.debug("Redis Service: Calling createJob... retry count " + RetrySynchronizationManager.getContext().getRetryCount());
+		URI uri = new URI(this.redisUrl + "job");
+
+		HttpEntity<Job> entity = new HttpEntity<Job>(job,
+				AuthHelper.createBasicAuthHeaders(props.getRedisClientUsername(), props.getRedisClientPassword()));
+
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity(uri, entity, String.class);
+
+		return CompletableFuture.completedFuture(responseEntity);
+	}
+
+	@Override
+	@Retryable(retryFor = RestClientException.class, maxAttemptsExpression = "${application.net.max.retries}", backoff = @Backoff(delayExpression = "${application.net.delay}"))
+	public CompletableFuture<ResponseEntity<String>> updateJob(Job job) throws Exception {
+		
+		logger.debug("Redis Service: Calling updateJob... retry count " + RetrySynchronizationManager.getContext().getRetryCount());
+		URI uri = new URI(this.redisUrl + "job");
+
+		HttpEntity<Job> entity = new HttpEntity<Job>(job,
+				AuthHelper.createBasicAuthHeaders(props.getRedisClientUsername(), props.getRedisClientPassword()));
+
+		ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.PUT, entity, String.class);
+
+		return CompletableFuture.completedFuture(responseEntity);
+	}
+
+	@Override
+	@Retryable(retryFor = RestClientException.class, maxAttemptsExpression = "${application.net.max.retries}", backoff = @Backoff(delayExpression = "${application.net.delay}"))
+	public CompletableFuture<ResponseEntity<String>> deleteJob(String jobId) throws Exception {
+		
+		/**
+		 * Returns 404 NOT FOUND if jobId doesn't exist. 
+		 */
+		
+		logger.debug("Redis Service: Calling deleteJob... retry count " + RetrySynchronizationManager.getContext().getRetryCount());
+		URI uri = new URI(this.redisUrl + "job/" + jobId);
+
+		HttpEntity<Job> entity = new HttpEntity<Job>(
+				AuthHelper.createBasicAuthHeaders(props.getRedisClientUsername(), props.getRedisClientPassword()));
+
+		ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.DELETE, entity, String.class);
+
+		return CompletableFuture.completedFuture(responseEntity);
+		
+	}
+	
+	/**
+	 * The following is the only Async method as it is called directly from the document controller. All other methods in this service are called from the 
+	 * Async Job processor which makes them Async automatically. 
+	 */
+	@Override
+	@Async			
 	@Retryable(retryFor = Exception.class, maxAttemptsExpression = "${application.net.max.retries}", backoff = @Backoff(delayExpression = "${application.net.delay}"))
 	public CompletableFuture<ResponseEntity<Job>> getJob(String jobId) throws Exception {
 		
@@ -102,59 +156,6 @@ public class RedisCacheClientServiceImpl implements RedisCacheClientService {
 		}
 
 		return CompletableFuture.completedFuture(responseEntity);
-	}
-
-	@Override
-	@Async
-	@Retryable(retryFor = RestClientException.class, maxAttemptsExpression = "${application.net.max.retries}", backoff = @Backoff(delayExpression = "${application.net.delay}"))
-	public CompletableFuture<ResponseEntity<String>> createJob(Job job) throws Exception {
-		
-		logger.debug("Redis Service: Calling createJob... retry count " + RetrySynchronizationManager.getContext().getRetryCount());
-		URI uri = new URI(this.redisUrl + "job");
-
-		HttpEntity<Job> entity = new HttpEntity<Job>(job,
-				AuthHelper.createBasicAuthHeaders(props.getRedisClientUsername(), props.getRedisClientPassword()));
-
-		ResponseEntity<String> responseEntity = restTemplate.postForEntity(uri, entity, String.class);
-
-		return CompletableFuture.completedFuture(responseEntity);
-	}
-
-	@Override
-	@Async
-	@Retryable(retryFor = RestClientException.class, maxAttemptsExpression = "${application.net.max.retries}", backoff = @Backoff(delayExpression = "${application.net.delay}"))
-	public CompletableFuture<ResponseEntity<String>> updateJob(Job job) throws Exception {
-		
-		logger.debug("Redis Service: Calling updateJob... retry count " + RetrySynchronizationManager.getContext().getRetryCount());
-		URI uri = new URI(this.redisUrl + "job");
-
-		HttpEntity<Job> entity = new HttpEntity<Job>(job,
-				AuthHelper.createBasicAuthHeaders(props.getRedisClientUsername(), props.getRedisClientPassword()));
-
-		ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.PUT, entity, String.class);
-
-		return CompletableFuture.completedFuture(responseEntity);
-	}
-
-	@Override
-	@Async
-	@Retryable(retryFor = RestClientException.class, maxAttemptsExpression = "${application.net.max.retries}", backoff = @Backoff(delayExpression = "${application.net.delay}"))
-	public CompletableFuture<ResponseEntity<String>> deleteJob(String jobId) throws Exception {
-		
-		/**
-		 * Returns 404 NOT FOUND if jobId doesn't exist. 
-		 */
-		
-		logger.debug("Redis Service: Calling deleteJob... retry count " + RetrySynchronizationManager.getContext().getRetryCount());
-		URI uri = new URI(this.redisUrl + "job/" + jobId);
-
-		HttpEntity<Job> entity = new HttpEntity<Job>(
-				AuthHelper.createBasicAuthHeaders(props.getRedisClientUsername(), props.getRedisClientPassword()));
-
-		ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.DELETE, entity, String.class);
-
-		return CompletableFuture.completedFuture(responseEntity);
-		
 	}
 
 	/**
